7745ee5c911e4ba9f5f6a39c93f4fa6d3f123c83
marcdelos <marc.delosaires01@estudiant.upf.edu>
2025-01-16 17:45:52 +0100

    added main and dockerfile to testserver 1 and 2

---
+++ projects/1-hdfs/testserver1/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class SumRequest(BaseModel):
+    x: int
+    y: int
+
+@app.post("/sum")
+def calculate_sum(request: SumRequest):
+    return {"result": request.x + request.y}
