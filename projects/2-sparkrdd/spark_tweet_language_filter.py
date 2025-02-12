ba972be313eeaa29a38aea84fdee29d1e8b52982
marcdelos <marc.delosaires01@estudiant.upf.edu>
2025-02-12 13:25:57 +0100

    Creates spark_tweet_retweets-py

--- projects/2-sparkrdd/spark_tweet_language_filter.py
+++ projects/2-sparkrdd/spark_tweet_language_filter.py
@@ -6,16 +6,13 @@ if len(sys.argv) != 4:
     print("Usage: spark_tweet_language_filter.py <language_code> <input_file> <output_file>")
     sys.exit(-1)
 
-# Get command-line arguments
 language = sys.argv[1]
 input_file = sys.argv[2]
 output_file = sys.argv[3]
 
-# Initialize Spark Context
 conf = SparkConf().setAppName("spark-tweet-language-filter")
 sc = SparkContext(conf=conf)
 
-# Read the tweets JSON file (assuming one JSON tweet per line)
 tweets = sc.textFile(input_file)
 
 def filter_by_language(line):
@@ -27,11 +24,8 @@ def filter_by_language(line):
         # If there's an error parsing the JSON, ignore this line
         return False
 
-# Filter tweets that match the provided language
 filtered_tweets = tweets.filter(filter_by_language)
 
-# Save the filtered tweets to the output directory.
-# Note: Spark will save the results in part files under the given directory.
 filtered_tweets.saveAsTextFile(output_file)
 
 sc.stop()
---
+++ projects/2-sparkrdd/spark_tweet_retweets.py
@@ -0,0 +1,35 @@
+from pyspark import SparkContext, SparkConf
+import sys
+import json
+
+if len(sys.argv) != 4:
+    print("Usage: spark_tweet_retweets.py <language_code> <input_file> <output_file>")
+    sys.exit(-1)
+
+language = sys.argv[1]
+input_file = sys.argv[2]
+output_file = sys.argv[3]
+
+conf = SparkConf().setAppName("spark-tweet-retweets")
+sc = SparkContext(conf=conf)
+
+tweets = sc.textFile(input_file)
+
+def safe_json_parse(line):
+    try:
+        return json.loads(line)
+    except Exception:
+        return None
+
+tweets_parsed = tweets.map(safe_json_parse).filter(lambda tweet: tweet is not None)
+
+tweets_filtered = tweets_parsed.filter(lambda tweet: tweet.get("lang") == language)
+
+top_10_tweets = tweets_filtered.sortBy(lambda tweet: tweet.get("retweet_count", 0), ascending=False) \
+    .zipWithIndex() \
+    .filter(lambda tup: tup[1] < 10) \
+    .map(lambda tup: tup[0])
+
+top_10_tweets.saveAsTextFile(output_file)
+
+sc.stop()
