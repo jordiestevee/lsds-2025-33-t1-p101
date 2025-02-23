63e802170da06c52e8bdf19c19e9dba7312cb212
Kowalski <albert.jane01@estudiant.upf.edu>
2025-02-23 22:10:36 +0100

    Black convention code format

--- projects/2-sparkrdd/spark_count_bigrams.py
+++ projects/2-sparkrdd/spark_count_bigrams.py
@@ -10,7 +10,12 @@ sc = SparkContext(conf=conf)
 lines_rdd = sc.textFile(source)
 
 # Split each line into words and generate bigrams
-bigrams_rdd = lines_rdd.flatMap(lambda line: [((line.split()[i], line.split()[i+1]), 1) for i in range(len(line.split()) - 1)])
+bigrams_rdd = lines_rdd.flatMap(
+    lambda line: [
+        ((line.split()[i], line.split()[i + 1]), 1)
+        for i in range(len(line.split()) - 1)
+    ]
+)
 
 # Count the occurrences of each bigram
 bigram_counts_rdd = bigrams_rdd.reduceByKey(lambda a, b: a + b)
